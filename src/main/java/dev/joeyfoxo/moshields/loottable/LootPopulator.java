9a6a3a4e5475f28f732f6dc53bd083de092a2df9
Joey <joey@ziax.com>
2024-02-06 15:09:39 +0000

    Sync

--- src/main/java/dev/joeyfoxo/moshields/loottable/LootPopulator.java
+++ src/main/java/dev/joeyfoxo/moshields/loottable/LootPopulator.java
@@ -1,4 +1,38 @@
 package dev.joeyfoxo.moshields.loottable;
 
+import dev.joeyfoxo.moshields.util.UtilClass;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LootPopulator {
+
+    public void populateLoot(Inventory inventory, ItemStack upgrade, double chance) {
+
+        if (!UtilClass.percentChance(chance)) {
+            return;
+        }
+
+        List<Integer> shuffleSlotList = getFreeSlotIndices(inventory);
+        Collections.shuffle(shuffleSlotList);
+
+        int slot = shuffleSlotList.get(0);
+        inventory.setItem(slot, upgrade);
+
+    }
+
+    public List<Integer> getFreeSlotIndices(Inventory inventory) {
+        List<Integer> freeSlotIndices = new ArrayList<>();
+        ItemStack[] contents = inventory.getContents();
+        for (int i = 0; i < contents.length; i++) {
+            if (contents[i] == null) {
+                freeSlotIndices.add(i);
+            }
+        }
+        return freeSlotIndices;
+    }
+
 }
--- src/main/java/dev/joeyfoxo/moshields/loottable/StructureListener.java
+++ src/main/java/dev/joeyfoxo/moshields/loottable/StructureListener.java
@@ -2,15 +2,17 @@ package dev.joeyfoxo.moshields.loottable;
 
 import dev.joeyfoxo.moshields.MoShields;
 import dev.joeyfoxo.moshields.upgrades.items.EchoUpgrade;
+import dev.joeyfoxo.moshields.upgrades.items.SlimeUpgrade;
+import dev.joeyfoxo.moshields.upgrades.items.SpikedUpgrade;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Chest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.generator.structure.Structure;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.StructureSearchResult;
 
@@ -20,28 +22,46 @@ public class StructureListener implements Listener {
         Bukkit.getPluginManager().registerEvents(this, JavaPlugin.getPlugin(MoShields.class));
     }
 
+    public boolean isStructureNearby(Location location, Structure structure, int radius) {
+        World world = location.getWorld();
+        StructureSearchResult structureSearchResult = world.locateNearestStructure(location, structure, radius, false);
+        return structureSearchResult != null;
+    }
+
     @EventHandler
     public void playerOpenChest(PlayerInteractEvent event) {
 
-        if (event.getClickedBlock() == null ||
-                !(event.getClickedBlock().getType() == Material.CHEST || event.getClickedBlock().getType() == Material.TRAPPED_CHEST) ||
-                !event.getAction().isRightClick()) {
+        LootPopulator populator = new LootPopulator();
+
+        if (event.getClickedBlock() == null) {
             return;
         }
 
-        Chest chest = (Chest) event.getClickedBlock().getState();
-        Location chestLocation = chest.getLocation();
-        World world = chestLocation.getWorld();
-        StructureSearchResult structureSearchResult = world.locateNearestStructure(chestLocation, Structure.ANCIENT_CITY, 10, false);
+        if (event.getClickedBlock().getState() instanceof Chest chest && event.getAction().isRightClick()) {
 
-        System.out.println(structureSearchResult);
+            if (chest.hasBeenFilled()) {
+                return;
+            }
 
-        if (structureSearchResult == null) {
-            return;
+            Location chestLocation = chest.getLocation();
+            Inventory inventory = chest.getBlockInventory();
+
+            if (isStructureNearby(chestLocation, Structure.ANCIENT_CITY, 10)) {
+                populator.populateLoot(inventory, EchoUpgrade.getUpgrade(), 1);
+                return;
+            }
+
+            if (isStructureNearby(chestLocation, Structure.JUNGLE_PYRAMID, 10)) {
+                populator.populateLoot(inventory, SlimeUpgrade.getUpgrade(), 0.04);
+                return;
+            }
+
+            if (isStructureNearby(chestLocation, Structure.BASTION_REMNANT, 10)) {
+                populator.populateLoot(inventory, SpikedUpgrade.getUpgrade(), 0.06);
+                return;
+            }
+
         }
-
-        chest.getBlockInventory().addItem(EchoUpgrade.getUpgrade());
-
     }
 
 }
